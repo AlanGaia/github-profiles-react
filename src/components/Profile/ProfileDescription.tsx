7c209be6a9a6dbbf9b3934759d425fb596c003f2
Alan <alan.j.gaia@gmail.com>
2020-11-29 22:22:24 -0300

    refactoring profile

--- src/components/Profile/ProfileDescription.tsx
+++ src/components/Profile/ProfileDescription.tsx
@@ -6,6 +6,9 @@ import * as ProfileServices from './ProfileServices';
 function ProfileDescription({ }) {
 
 
+  let username = 'AlanGaia';
+
+
 
   const initialState = {
     bio: '',
@@ -26,10 +29,16 @@ function ProfileDescription({ }) {
 
 
 
+  const Subtitles = styled.h3`
+    color: #ffffff;
+    text-align: left;
+    font-size: 1.5rem;
+    margin-left: 1rem;
+  `;
 
   const ContainerProfile = styled.div`
     padding: 1rem;
-    background-color: #00423d;
+    background-color: #22305e;
     margin: 1rem;
     display: flex;
     flex-direction:column;
@@ -40,29 +49,35 @@ function ProfileDescription({ }) {
 
   const ContainerRow = styled.div`
     display:flex;
-    flex-direction: row;
-    justify-content: space-between;
+    flex-direction: column;
+    justify-content: center;
   `;
 
   const Name = styled.h2`
     color: #ffffff;
-    font-size: 2rem;
+    font-size: 2.5rem;
     font-weight: bold;
     text-align: center;
   `;
 
   const HalfContainer = styled.div`
-    max-width: 50%;
-    background-color: #1d615b;
+    background-color: #394f97;
     margin: 1rem;
+    margin-top: 0px;
     padding: 1rem;
-
+    color: #ffffff;
+    align-items: center;
+    justify-content: space-between;
+    display:flex;
+    flex-direction: row;
   `;
+  
+
 
 
   const Avatar = styled.img`
     vertical-align: middle;
-    width: 200px;
+    width: 150px;
     border-radius: 50%;
   `;
 
@@ -71,14 +86,23 @@ function ProfileDescription({ }) {
     <ContainerProfile>
       <Name>{user.name}</Name>
       <ContainerRow>
+      <Subtitles>About</Subtitles>
       <HalfContainer>
         <Avatar src={user.avatar_url} />
         <p>{user.bio}</p>
       </HalfContainer>
+      <Subtitles>My repos</Subtitles>
       <HalfContainer>
+        {
+          (user.repos_url) ?
+          <button>Get the repos</button> :
+          null
+        }
         <p>{user.repos_url}</p>
       </HalfContainer>
       </ContainerRow>
+
+      <input type="text" value={username}/>
       
       <button onClick={() => getUser('AlanGaia')
       }>Get the user</button>
